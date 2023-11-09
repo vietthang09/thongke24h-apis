00a1c7796a3491865da0e7f95c4a0eb96ac305ed
vietthang09 <vthcvn@gmail.com>
2023-11-09 07:50:13 +0700

    feat: update db connection

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  // user: "thongke24h",
-  // host: "153.92.222.161",
-  // password: "vZmGdTrkkJ3V4b2YAgUg",
-  // database: "thongke24h",
-  user: "root",
-  host: "localhost",
-  password: "",
+  user: "thongke24h",
+  host: "153.92.222.161",
+  password: "vZmGdTrkkJ3V4b2YAgUg",
   database: "thongke24h",
+  // user: "root",
+  // host: "localhost",
+  // password: "",
+  // database: "thongke24h",
 });
 
 module.exports = db;
